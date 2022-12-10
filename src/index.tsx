e8158d7558e9b3840e882e8f38aaf53c51688768
ashot1arzumanyan <ashot1arzumanyan@gmail.com>
2022-12-10 11:17:47 +0400

    rename sass filenames

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import './styles/index.scss';
+import './styles/styles.scss';
 import 'pdfjs-dist/web/pdf_viewer.css';
 
 const root = createRoot(document.getElementById('root'));
